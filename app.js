15ed1a89743ebc65dee1114c0d90d86e230df866
maciek <42122011+xkcm@users.noreply.github.com>
2022-06-28 13:55:27 +0200

    KP-787 Version 1.0.0 (#1)

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
-function hello(name) {
-  const greeting = `hello ${name}`;
-  return greeting;
+const kaholoPluginLibrary = require("@kaholo/plugin-library");
+const SmtpMailService = require("./smtp-mail-service");
+
+function send({
+  hostname,
+  port,
+  username,
+  password,
+  from,
+  to,
+  subject,
+  cc,
+  bcc,
+  text,
+  html,
+  attachmentPaths,
+}) {
+  const smtpMailService = new SmtpMailService(hostname, port, username, password);
+
+  return smtpMailService.send({
+    from,
+    to,
+    subject,
+    cc,
+    bcc,
+    text,
+    html,
+    attachmentPaths,
+  });
 }
 
-module.exports = {
-  hello,
-};
+module.exports = kaholoPluginLibrary.bootstrap({
+  send,
+});
